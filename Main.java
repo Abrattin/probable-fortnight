f42b6df0e364b95f311855fe062b743c7b226d17
abrattin <89173811+Abrattin@users.noreply.github.com>
2022-11-17 16:20:30 -0600

    ref types

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
+import java.util.class;
 public class Main {
 	public static void main(String[] args) {
 		int myAge = 30;
 		int herAge = 31;
 		byte age = 30;
 		long viewsCount = 3_123_456_789L;
-		System.out.println(viewsCount);
+
 		//float price 10.99F;
-		
+		char letter = 'A';
+		boolean isEligible = false;
+		System.out.println(isEligible);
 	  }
 	  
 	}
