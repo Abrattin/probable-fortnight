f7521fb792418ef94851a8e1ff2bef03de9e9034
abrattin <89173811+Abrattin@users.noreply.github.com>
2022-11-17 16:13:26 -0600

    variable refresher

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		int myAge = 30;
+		int herAge = 31;
+		byte age = 30;
+		long viewsCount = 3_123_456_789L;
+		System.out.println(viewsCount);
+		//float price 10.99F;
+		
+	  }
+	  
+	}
